6ce78d70f9a2f37eb49b7fcf8941b1e4cdc83829
kanta13jp1 <kanta13jp@gmail.com>
2022-02-05 20:50:37 +0900

    CSV読み込み

--- sample/SampleViewController.swift
+++ sample/SampleViewController.swift
@@ -9,15 +9,37 @@ import UIKit
 
 class SampleViewController: UIViewController {
 
+    var csvArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        csvArray = loadCSV(fileName: "quiz")
+        print(csvArray)
+        
         // Do any additional setup after loading the view.
     }
     
     // ボタンを押したときに呼ばれる
     @IBAction func btnAction(sender: UIButton) {
         print(sender.tag)
+    }
+    
+    func loadCSV(fileName: String) -> [String] {
+        print(fileName)
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            let csvData = try String(contentsOfFile: csvBundle, encoding: String.Encoding.utf8)
+            print(csvData)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            print(lineChange)
+            csvArray = lineChange.components(separatedBy: "\n")
+            print(csvArray)
+            csvArray.removeLast()
+        } catch {
+            print("エラー")
+        }
+        return csvArray
     }
     
     /*
