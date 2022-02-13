d7dad1f88b46f89a929a5e7f275e1e639d11d170
kanta13jp1 <kanta13jp@gmail.com>
2022-02-13 09:28:43 +0900

    タブにアイコンを表示

--- sample/ViewController.swift
+++ sample/ViewController.swift
@@ -14,9 +14,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         startButton.layer.borderWidth = 2
         startButton.layer.borderColor = UIColor.black.cgColor
-        // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UITabBar.appearance().tintColor = UIColor.red
+        
+        self.setTabBarItem(
+            index:0,
+            titile: "クイズ",
+            image: UIImage(named: "tab_home_off")!,
+            selectedImage: UIImage(named: "tab_home_on")!,
+            offColor: UIColor.blue,
+            onColor: UIColor.red)
+        self.setTabBarItem(
+            index:1,
+            titile: "時計",
+            image: UIImage(named: "tab_home_off")!,
+            selectedImage: UIImage(named: "tab_home_on")!,
+            offColor: UIColor.blue,
+            onColor: UIColor.red)
+        self.setTabBarItem(
+            index:2,
+            titile: "ストップウォッチ",
+            image: UIImage(named: "tab_home_off")!,
+            selectedImage: UIImage(named: "tab_home_on")!,
+            offColor: UIColor.blue,
+            onColor: UIColor.red)
+    }
 
+    func setTabBarItem(index: Int, titile: String, image: UIImage, selectedImage: UIImage, offColor: UIColor, onColor: UIColor) -> Void {
+        let tabBarItem = self.tabBarController?.tabBar.items![index]
+        tabBarItem!.title = title
+        tabBarItem!.image = image.withRenderingMode(.alwaysOriginal)
+        tabBarItem!.selectedImage = selectedImage.withRenderingMode(.alwaysOriginal)
+        tabBarItem!.setTitleTextAttributes([ . foregroundColor: offColor], for: .normal)
+        tabBarItem!.setTitleTextAttributes([ . foregroundColor: onColor], for: .selected)
+    }
 }
 
